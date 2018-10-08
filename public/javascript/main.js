5c8356e075b8f17eba25f60744aefe24b0895f0c
Margarita Barbotiko <mbarbotiko@gmail.com>
2018-10-08 07:58:42 -0500

    setting up game object

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,4 +1,29 @@
-$(document).ready(function () {
+let hangManGameObj={
 
-});
+    wordsToPick:{
 
+        genesis: {
+            picture: "genesis.jpg",
+            song: "Illegal Alien",
+            preview: "https://p.scdn.co/mp3-preview/b29a2b925b9654e0efaaff37504fd234307e0ad8"
+          },
+          madonna: {
+            picture: "madonna.jpg",
+            song: "Material Girl",
+            preview: "https://p.scdn.co/mp3-preview/5ff7f7b7d2af1a747da275bed3c49054c01b5b1c"
+          },
+          toto: {
+            picture: "toto.jpg",
+            song: "Rosanna",
+            preview: "https://p.scdn.co/mp3-preview/7cef811eaeb7c7b98245750e73d9d68e9008f317"
+          }
+    }
+
+
+
+}
+
+console.log(hangManGameObj);
+console.log(hangManGameObj.wordsToPick);
+console.log(hangManGameObj.wordsToPick.genesis);
+console.log(hangManGameObj.wordsToPick.genesis.song);
