2f07dbb28365309c82cd3383ebde998058a439be
Margarita Barbotiko <mbarbotiko@gmail.com>
2018-10-09 07:51:10 -0500

    adding guess array test and underscore function evaluating values in arrays against eachother

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,50 +1,72 @@
-let hangManGameObj = {
-
-    wordsToPick: {
 
 
-
-        genesis: {
-            picture: "genesis.jpg",
-            song: "Illegal Alien",
-            preview: "https://p.scdn.co/mp3-preview/b29a2b925b9654e0efaaff37504fd234307e0ad8"
-        },
-        madonna: {
-            picture: "madonna.jpg",
-            song: "Material Girl",
-            preview: "https://p.scdn.co/mp3-preview/5ff7f7b7d2af1a747da275bed3c49054c01b5b1c"
-        },
-        toto: {
-            picture: "toto.jpg",
-            song: "Rosanna",
-            preview: "https://p.scdn.co/mp3-preview/7cef811eaeb7c7b98245750e73d9d68e9008f317"
-        }
-
-
+var wordsToPick = {
+    genesis: {
+        picture: "genesis.jpg",
+        song: "Illegal Alien",
+        preview: "https://p.scdn.co/mp3-preview/b29a2b925b9654e0efaaff37504fd234307e0ad8"
     },
+    madonna: {
+        picture: "madonna.jpg",
+        song: "Material Girl",
+        preview: "https://p.scdn.co/mp3-preview/5ff7f7b7d2af1a747da275bed3c49054c01b5b1c"
+    },
+    toto: {
+        picture: "toto.jpg",
+        song: "Rosanna",
+        preview: "https://p.scdn.co/mp3-preview/7cef811eaeb7c7b98245750e73d9d68e9008f317"
+    }
 
 }
+
+
 
 // console.log(hangManGameObj);
 // console.log(hangManGameObj.wordsToPick);
 // console.log(hangManGameObj.wordsToPick.genesis);
-// console.log(hangManGameObj.wordsToPick.genesis.song);
+// console.log(hangManGameObj.wordsToPick.genesis.song); testing accessing the object
 
 let currentWord = null;
 let lettersInWord = [];
-let correctGuessLetters = [];
+let correctGuessLetters = ['a','i', 'o', 'u', 'z'];
 let allGuessedLetters = [];
 let guessesLeft = 0;
 let totalGuesses = 0;
 let currentGuessedLetter = null;
 wins = 0;
 
+//setting up variables to hold values during game play
+
 function randomWord() {
-    let wordsKey = Object.keys(hangManGameObj.wordsToPick);
-    console.log(wordsKey);
-    hangManGameObj.currentWord = wordsKey[Math.floor(Math.random() * wordsKey.length)];
-    console.log(hangManGameObj.currentWord);
+    let wordsKey = Object.keys(wordsToPick);
+    // console.log(wordsKey);
+    //creating an array from the words Key Object / changing options to string
+    wordsToPick.currentWord = wordsKey[Math.floor(Math.random() * wordsKey.length)];
+    // console.log(wordsToPick.currentWord);
+    //setting current word in play to a random value in the array
+    lettersInWord = wordsToPick.currentWord.split('');
+    console.log(lettersInWord);
 
 }
 
 randomWord();
+
+function letterUnderScore() {
+    let underScore = " ";
+    // console.log(underScore);
+    for (var i = 0; i < lettersInWord.length; i++)
+    console.log(correctGuessLetters.indexOf(lettersInWord[i])!==-1);
+    //loop through the letters in the word array
+    if (correctGuessLetters.indexOf(lettersInWord[i]) !==-1 ){
+        console.log(lettersInWord[i]);
+        console.log(correctGuessLetters.indexOf(lettersInWord[i])!==-1);
+        //check each letter against the correctly guessed array of letters find the first occurance in letters in word array index 
+        underScore +=lettersInWord[i];
+    }else{
+        underScore+="  _  ";
+        // console.log(underScore);
+        //if the letter doesnt occur add an underscore
+    }
+}
+
+letterUnderScore();
