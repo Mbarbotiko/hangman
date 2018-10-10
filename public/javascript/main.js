495a16a46f49cc0141898a2ca2054b2a334fff19
Margarita Barbotiko <mbarbotiko@gmail.com>
2018-10-10 09:12:21 -0500

    adding value to guesses left

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -6,29 +6,23 @@ var wordsToPick = {
         song: "Illegal Alien",
         preview: "https://p.scdn.co/mp3-preview/b29a2b925b9654e0efaaff37504fd234307e0ad8"
     },
-    madonna: {
-        picture: "madonna.jpg",
-        song: "Material Girl",
-        preview: "https://p.scdn.co/mp3-preview/5ff7f7b7d2af1a747da275bed3c49054c01b5b1c"
-    },
-    toto: {
-        picture: "toto.jpg",
-        song: "Rosanna",
-        preview: "https://p.scdn.co/mp3-preview/7cef811eaeb7c7b98245750e73d9d68e9008f317"
-    }
+    // madonna: {
+    //     picture: "madonna.jpg",
+    //     song: "Material Girl",
+    //     preview: "https://p.scdn.co/mp3-preview/5ff7f7b7d2af1a747da275bed3c49054c01b5b1c"
+    // },
+    // toto: {
+    //     picture: "toto.jpg",
+    //     song: "Rosanna",
+    //     preview: "https://p.scdn.co/mp3-preview/7cef811eaeb7c7b98245750e73d9d68e9008f317"
+    // }
 
 }
 
 
-
-// console.log(hangManGameObj);
-// console.log(hangManGameObj.wordsToPick);
-// console.log(hangManGameObj.wordsToPick.genesis);
-// console.log(hangManGameObj.wordsToPick.genesis.song); testing accessing the object
-
 let currentWord = null;
 let lettersInWord = [];
-let correctGuessLetters = ['a','i', 'o', 'u', 'z'];
+let correctGuessLetters = ['g', 's'];
 let allGuessedLetters = [];
 let guessesLeft = 0;
 let totalGuesses = 0;
@@ -46,27 +40,46 @@ function randomWord() {
     //setting current word in play to a random value in the array
     lettersInWord = wordsToPick.currentWord.split('');
     console.log(lettersInWord);
+    letterUnderScore();
+
+    
 
 }
 
-randomWord();
-
 function letterUnderScore() {
-    let underScore = " ";
+    let underScore = ' ';
     // console.log(underScore);
     for (var i = 0; i < lettersInWord.length; i++)
-    console.log(correctGuessLetters.indexOf(lettersInWord[i])!==-1);
+
     //loop through the letters in the word array
     if (correctGuessLetters.indexOf(lettersInWord[i]) !==-1 ){
-        console.log(lettersInWord[i]);
-        console.log(correctGuessLetters.indexOf(lettersInWord[i])!==-1);
-        //check each letter against the correctly guessed array of letters find the first occurance in letters in word array index 
+        // console.log(lettersInWord[i]);
+        //only i is present in this array after looping through and crossreferencing one array vs the other
+        // console.log(correctGuessLetters.indexOf(lettersInWord[i])!==-1);
+        //if we have a match (evaluates as TRUE and NOT FALSE (-1))
+        // Evaluates against -1 which is an undefined index in the array, if not -1 print true, else print false.
         underScore +=lettersInWord[i];
+        console.log(underScore);
     }else{
         underScore+="  _  ";
         // console.log(underScore);
         //if the letter doesnt occur add an underscore
     }
+    document.getElementById("current-word").innerHTML = underScore;
 }
 
-letterUnderScore();
+function increaseTotalGuesses(){
+    totalGuesses = lettersInWord.length+10;
+    guessesLeft = totalGuesses;
+    console.log(guessesLeft);
+    document.querySelector("#guesses-remaining").innerHTML = guessesLeft;
+    //give the player 10 additional guesses beyond the number of letters in the word and update DOM.
+
+}
+
+increaseTotalGuesses();
+
+
+
+
+
